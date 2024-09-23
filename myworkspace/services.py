0a79bc89e6cc9b1ee376818e12df8b92025864a2
Engel Pinto <engeljavierpinto@gmail.com>
2024-09-23 15:30:29 -0400

    feat: Se crea la estructura de directorio completa
    Se anexan hijos de primer nivel
    
    close #7

--- myworkspace/services.py
+++ myworkspace/services.py
@@ -16,14 +16,18 @@ class WorkspaceRepository:
         with open(path, "r") as f:
             data = json.load(f)
             for workspace in data:
-                workspaces.append(Workspace(name=workspace["name"]))
+                work = Workspace(name=workspace["name"])
+                if "children" in workspace:
+                    for child in workspace["children"]:
+                        work.add_child(Workspace(name=child["name"]))
+                workspaces.append(work)
         return workspaces
 
 
 class WorkspaceService:
     def __init__(self, name: str):
         self.name = name
-        self.repository = WorkspaceRepository()
+        self.repository = WorkspaceRepository(path=os.getcwd())
 
     def build(self) -> Workspace:
         workspace = Workspace(name=self.name)
